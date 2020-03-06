6dbeac9235b4827fd9da4e37e7be78ac79a82fc9
Jess Novak <55854760+meowyadoin@users.noreply.github.com>
2020-03-06 13:15:17 -0800

    Create users.test.js

---
+++ api/users/users.test.js
@@ -0,0 +1,9 @@
+const request = require("supertest");
+
+const server = require("../server");
+
+describe("users router", function() {
+    it("should display a list of users", function() {
+      expect(true).toBe(true);
+    })
+});
